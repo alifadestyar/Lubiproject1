d7a64e45478dbaf2f007a6258e10dcb1060be0b0
alifadestyar <alifadestyar12@gmail.com>
2026-04-05 08:44:57 +0700

    Array

---
+++ src/main/java/com/mycompany/lubiproject1/NilaiMahasiswa.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lubiproject1;
+import java.util.Scanner;
+/**
+ *
+ * @author ASUS
+ */
+public class NilaiMahasiswa {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+            int jumlahMahasiswa = 2;
+            int jumlahMapel = 3;
+            
+            int[][] nilai = new int[jumlahMahasiswa][jumlahMapel];
+            
+            for(int i=0; i<jumlahMahasiswa; i++){
+                System.out.println("--- Input Data Mahasiswa ke-" + (i+1) + " --- ");
+                for(int j=0; j < junmlahMapel; j++){
+                    System.out.print("Masukkan nilai Mapel ke-" + (j+1) + ": ");
+                    nilai[i][j]= scan.nextInt();
+                }
+            }
+            System.out.println("\n==========================");
+            System.out.println("    LAPORAN NILAI SISWA     ");
+            System.out.println("\n==========================");
+            
+            for(intj=0; j<jumlahMapel; j++){
+                System.out.print(nilai[i][j] + " | ");
+                total += nilai[i][j];
+            }
+            double rata = (double) total / jumlahMapel;
+            System.out.println("Rata-rata: " +rata);
+        }
+    scan.close();
+}
