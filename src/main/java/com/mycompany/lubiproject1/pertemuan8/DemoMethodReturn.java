301ddf8c2c95e983a0cc4b9770f2cb88666a1d10
alifadestyar <alifadestyar12@gmail.com>
2026-04-24 23:01:41 +0700

    Pertemuan 8

---
+++ src/main/java/com/mycompany/lubiproject1/pertemuan8/DemoMethodReturn.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lubiproject1.pertemuan8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DemoMethodReturn {
+    static int hitungLuas(int panjang, int lebar) {
+        return panjang * lebar;
+    }
+
+    static boolean isGenap(int angka) {
+        return (angka % 2 == 0);
+    }
+
+    public static void main(String[] args) {
+        int luas = hitungLuas(10, 5);
+        System.out.println("Luas Persegi Panjang: " + luas);
+        System.out.println("Luas Lain: " + hitungLuas(7, 3));
+
+        int nilai = 10;
+        if (isGenap(nilai)) {
+            System.out.println(nilai + " adalah Genap");
+        } else {
+            System.out.println(nilai + " adalah Ganjil");
+        }
+    }
+}
---
+++ src/main/java/com/mycompany/lubiproject1/pertemuan8/DemoMethodVoid.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.lubiproject1.pertemuan8;
+
+/**
+ *
+ * @author ASUS
+ */
+public class DemoMethodVoid {
+    static void garis() {
+        System.out.println("================================");
+    }
+
+    static void sapaTeman(String nama) {
+        System.out.println("Halo, " + nama + "! Selamat datang di Java.");
+    }
+
+    public static void main(String[] args) {
+        garis();
+        sapaTeman("Budi");
+        sapaTeman("Siti");
+        garis();
+    }
+}
